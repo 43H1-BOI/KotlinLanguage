6c07bdca44b3c05ca047e4b439dae3755b3b3186
Abhishek Yadav <153166442+43H1-BOI@users.noreply.github.com>
2025-02-26 00:05:45 +0530

    Optimized Rock Paper Scissors

--- src/Antiques/RockPaperScissors.kt
+++ src/Antiques/RockPaperScissors.kt
@@ -1,26 +1,13 @@
 package Antiques
 
-import kotlin.random.Random
-
 fun main() {
-
-
     println("Enter Your Choice :\n1. Rock\n2. Paper\n3. Scissors\nSelect Any One from Above :")
     val userInput = readln().toInt()
     val systemInput = (1..3).random()
 
     println("\n\n")
-    when (userInput) {
-        1 -> println("You Selected Rock")
-        2 -> println("You Selected Paper")
-        else -> println("You Selected Scissors") // 3
-    }
-    when (systemInput) {
-        1 -> println("System Selected Rock")
-        2 -> println("System Selected Paper")
-        else -> println("System Selected Scissors") // 3
-    }
-
+    input("You",userInput)
+    input("System",systemInput)
 
     if (userInput == 1 && systemInput == 3) {
         println("Hurray ! You Won")
@@ -32,5 +19,13 @@ fun main() {
         println("Match Draw")
     } else {
         println("System Won !")
+    }
+}
+
+fun input(inputBy: String, value: Int) {
+    when (value) {
+        1 -> println("$inputBy Selected Rock")
+        2 -> println("$inputBy Selected Paper")
+        else -> println("$inputBy Selected Scissors") // 3
     }
 }
